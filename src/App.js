7e8e94a3e3d0b073bb5033f5ec033731916923af
tanmay007thor <tanmayrathod777@gmail.com>
2024-01-20 20:12:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+// import HeroSection from "./components/Hero/HeroSection";
+// import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      router is working now
+      <BrowserRouter>
+        <Routes>
+          <Route path="/new" element={<>{/* <HeroSection/> */}</>}></Route>
+          <Route
+            path="/sd"
+            element={
+              <>
+                <Home />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      {/* <Footer /> */}
     </div>
   );
 }
